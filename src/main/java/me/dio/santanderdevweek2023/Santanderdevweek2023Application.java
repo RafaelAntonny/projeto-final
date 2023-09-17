b31689bd47942b8b6f5b8a80e1244ad4dc425d0a
Rafael Antonny <rafaelantonny6@gmail.com>
2023-09-17 03:53:37 -0300

    Initial commit

---
+++ src/main/java/me/dio/santanderdevweek2023/Santanderdevweek2023Application.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevweek2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Santanderdevweek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Santanderdevweek2023Application.class, args);
+	}
+
+}
